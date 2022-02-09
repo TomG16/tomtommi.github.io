4aa3ead746d1fea068070f8f46357423a8048e52
TomG16 <t.gossen02@student.alfa-college.nl>
2022-02-09 15:52:19 +0100

    Buttons functionality
    Buttons now have an actual functionality (may be a bit wonky on mobile, working on a fix.)

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 $(document).ready(function() {
+    
+    // 3 buttons functionality
     $('#button1').click(function(){
-        alert("Work In progress");
+        window.location.href = 'https://tomg16.github.io/tomtommi.github.io/';
     });
 
     $('#button2').click(function(){
-        alert("Work In progress");
+        $('#about_popup').show();
     });
 
     $('#button3').click(function(){
-        alert("Work In progress");
+        window.location.href = 'https://trello.com/b/fNaS6EW5/hub';
     });
+    $('.closebuttonx').click(function(){
+        $("#about_popup").hide();
+    });
+    
+    
 });
